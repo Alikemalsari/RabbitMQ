93653f760b8b5a23285527dc1c5dfca3564bebd1
Ali Kemal SARI <a.kemalsari46@gmail.com>
2024-04-27 09:47:59 +0300

    Created

---
+++ RabbitMQ/RabbitMQ/Program.cs
@@ -0,0 +1,29 @@
+﻿// Producer Kodu
+using RabbitMQ.Client;
+using System.Text;
+
+
+for (int i = 0; i < 150; i++)
+{
+    mesajilet();
+}
+
+Console.ReadLine();
+
+
+void mesajilet()
+{
+    var factory = new ConnectionFactory
+    {
+        HostName = "localhost",
+    };
+    using var connection = factory.CreateConnection();
+    var channel = connection.CreateModel();
+    channel.QueueDeclare("mesajKuyruk", durable: true, exclusive: false, autoDelete: false);
+    var mesaj = "İlk mesaj";
+
+    var body = Encoding.UTF8.GetBytes($"{mesaj} - Gönderme zamanı: {DateTime.Now}");
+    channel.BasicPublish(exchange: "", routingKey: "mesajKuyruk", body: body);
+
+    Console.WriteLine("Mesaj Kuyruğa iletildi.");
+}
---
+++ RabbitMQ/RabbitMQConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿// Consumer Kodu
+using RabbitMQ.Client.Events;
+using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("mesajKuyruk", durable: true, exclusive: false, autoDelete: false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("mesajKuyruk", true, consumer);
+consumer.Received += Consumer_Received;
+Console.ReadLine();
+
+
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var alinanMesaj = Encoding.UTF8.GetString(e.Body.ToArray());
+    var zamanDamgasiIndex = alinanMesaj.LastIndexOf("- Gönderme zamanı:");
+    var mesaj = alinanMesaj.Substring(0, zamanDamgasiIndex).Trim();
+    var zamanDamgasi = alinanMesaj.Substring(zamanDamgasiIndex + 1).Trim();
+
+    Console.WriteLine($"Gelen mesaj: {mesaj}");
+    Console.WriteLine($"Mesajın gönderme zamanı: {zamanDamgasi}");
+}
